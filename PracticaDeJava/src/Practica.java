de93a0aab045860596d2887bcb8beb530fdf381a
nano_ <nano_@NanoPC>
2017-11-19 18:57:21 -0400

    First Commit Testing.

---
+++ PracticaDeJava/src/Practica.java
@@ -0,0 +1,8 @@
+
+public class Practica {
+	public static void main(String[] args) {
+		
+		System.out.println("Bienvenidos, este programa se hizo solo para probar repositorios en GitHub");
+	}
+
+}
